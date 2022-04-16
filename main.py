e4409583a12a810984a8dd66bf50a6683249c3f0
dembskii <dembsky09@gmail.com>
2022-04-17 01:23:24 +0200

    Current weather
    - new icons
    - current weather api implemented
    - wind gust changes depending on wind direction

--- main.py
+++ main.py
@@ -8,6 +8,25 @@ import requests
 import datetime
 
 
+
+
+weather_symbols = {
+    "Clouds" : '<i style="color: white; padding-top:20px;" class="fa-solid fa-cloud fa-5x"></i>',
+    "Clear" : '<i style="color: orange;" class="fa-solid fa-sun fa-5x"></i>',
+    "Mist" : '<i style="color: blue;" class="fa-solid fa-smog fa-5x"></i>',
+    "Haze" : '<i style="color: blue;" class="fa-solid fa-smog fa-5x"></i>',
+    "Dust" : '<i style="color: blue;" class="fa-solid fa-smog fa-5x"></i>',
+    "Fog" : '<i style="color: blue;" class="fa-solid fa-smog fa-5x"></i>',
+    "Sand" : '<i style="color: blue;" class="fa-solid fa-wind fa-5x"></i>',
+    "Squall" : '<i style="color: blue;" class="fa-solid fa-wind fa-5x"></i>',
+    "Tornado" : '<i style="color: blue;" class="fa-solid fa-tornado fa-5x"></i>',
+    "Snow" : '<i style="color: blue;" class="fa-solid fa-snowflake fa-5x"></i>',
+    "Rain" : '<i style="color: blue;" class="fa-solid fa-cloud-rain fa-5x"></i>',
+    "Drizzle" : '<i style="color: blue;" class="fa-solid fa-cloud-rain fa-5x"></i>',
+    "Thunderstorm" : '<i style="color: blue;" class="fa-solid fa-cloud-bolt fa-5x"></i>'
+}
+
+
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 
@@ -33,9 +52,15 @@ def get_weather(location):
     data = response.json()
     print(response.url)
     current = data['current']
+    current['dt'] = str(datetime.datetime.fromtimestamp(current['dt'])).split(" ")[0]
     current['sunrise'] = str(datetime.datetime.fromtimestamp(current['sunrise'])).split(" ")[1].split(":")[:2]
+    current['sunrise'] = ":".join(current['sunrise'])
+    current['weather'][0]['description'] = current['weather'][0]['description'].capitalize()
+    current['visibility'] = int(current['visibility']/1000)
+   
+    
     current['sunset'] = str(datetime.datetime.fromtimestamp(current['sunset'])).split(" ")[1].split(":")[:2]
-
+    current['sunset'] = ":".join(current['sunset'])
     hourly = data['hourly'][:24]
     for hour in hourly:
         date = str(datetime.datetime.fromtimestamp(hour['dt'])).split(" ")[1]
@@ -54,12 +79,13 @@ def home():
 def weather():
     location = default_location
     if request.method == "POST":
-        location = request.form['location']
+        location = request.form['location'].capitalize()
+    weather = get_weather(location=location)
+    weather_current = weather[0]
+    weather_hourly = weather[1]
 
-    weather_current =get_weather(location=location)[0]
-
-
-    return render_template('index.html',location=location,weather=str(weather_current))
+    print(weather_current)
+    return render_template('index.html',location=location,weather_current=weather_current,weather_hourly=weather_hourly,symbols=weather_symbols)
 
 
 # Run app
