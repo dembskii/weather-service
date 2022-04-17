e0e8d6e2158b31271d4de81f5efbac431fed69d7
dembskii <dembsky09@gmail.com>
2022-04-17 16:29:33 +0200

    Localtime
    - hourly weather, sunset, sunrise  -> show localtime
    - optimized conversion
    - static.py -> new url endpoints

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask,render_template,redirect,url_for,request, flash
-from static import weather_api_endpoint
+from flask import Flask,render_template,redirect,url_for, request, flash
+from static import weather_api_endpoint,geocoder_api_endpoint, timezone_api_endpoint
 from flask_bootstrap import Bootstrap
 import os
 import requests
 import datetime
+import pytz
 
 
 
@@ -15,7 +16,7 @@ default_location = "Gdynia"
 
 def get_weather(location):
 
-    response = requests.get(url="http://api.openweathermap.org/geo/1.0/direct?",params={"q":location,"limit":1,"appid": os.environ.get("APP_ID")})
+    response = requests.get(url=geocoder_api_endpoint,params={"q":location,"limit":1,"appid": os.environ.get("APP_ID")})
   
     lon = response.json()[0]["lon"]
     lat = response.json()[0]["lat"]
@@ -32,26 +33,48 @@ def get_weather(location):
     response = requests.get(weather_api_endpoint,params=params)
     data = response.json()
     print(response.url)
-
     current = data['current']
-   
-    current['dt'] = str(datetime.datetime.fromtimestamp(current['dt'])).split(" ")[0]
-    current['sunrise'] = str(datetime.datetime.fromtimestamp(current['sunrise'])).split(" ")[1].split(":")[:2]
-    current['sunrise'] = ":".join(current['sunrise'])
+    utc_timezone= pytz.timezone("Europe/London")
+
+    response = requests.get(url=timezone_api_endpoint,params={"apiKey":os.environ.get("TIME_ZONE_API_KEY"),"lat":lat,"long":lon})
+    timezone_data = response.json()
+
+    time = datetime.datetime.fromtimestamp(current['dt'],utc_timezone)
+
+    offset = timezone_data['timezone_offset_with_dst'] - int(str(time).split(" ")[1].split(":")[2][3:])
+
+
+    for i in ["sunrise","sunset","dt"]:
+        time = datetime.datetime.fromtimestamp(current[i],utc_timezone)
+        if offset < 0:
+            time = time - datetime.timedelta(hours=abs(offset))
+        elif offset > 0:
+            time = time + datetime.timedelta(hours=offset)
+        
+        time= str(time).split(" ")[1].split(":")[:2]
+        time = ":".join(time)
+
+        current[i] = time
+
     current['weather'][0]['description'] = current['weather'][0]['description'].capitalize()
     current['visibility'] = int(current['visibility']/1000)
     current['wind_speed'] = int(current['wind_speed'])
     current['temp'] = int(current['temp'])
     
-    current['sunset'] = str(datetime.datetime.fromtimestamp(current['sunset'])).split(" ")[1].split(":")[:2]
-    current['sunset'] = ":".join(current['sunset'])
     hourly = data['hourly'][:24]
 
     for hour in hourly:
-        date = str(datetime.datetime.fromtimestamp(hour['dt'])).split(" ")[1].split(":")[:2]
-        date = ":".join(date)
+        time = datetime.datetime.fromtimestamp(hour['dt'],utc_timezone)
+        if offset < 0:
+            time = time - datetime.timedelta(hours=abs(offset))
+        elif offset > 0:
+            time = time + datetime.timedelta(hours=offset)
+        
+        time= str(time).split(" ")[1].split(":")[:2]
+        time = ":".join(time)
+        
         hour['temp'] = round(hour['temp'])
-        hour['dt'] = date
+        hour['dt'] = time
 
     return current,hourly
 
@@ -78,7 +101,7 @@ def weather():
         flash(f"Our service does not provide in {location}")
         return redirect(url_for("weather"))
 
-    print(weather_current)
+    
     return render_template('index.html',location=location,weather_current=weather_current,weather_hourly=weather_hourly)
 
 
--- static.py
+++ static.py
@@ -1,1 +1,3 @@
 weather_api_endpoint= "https://api.openweathermap.org/data/2.5/onecall?"
+geocoder_api_endpoint="http://api.openweathermap.org/geo/1.0/direct?"
+timezone_api_endpoint = 'https://api.ipgeolocation.io/timezone?'
